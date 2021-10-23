94b1b7fc42cf4d5d5fceaee66fb4c31fb254211a
hiro881213 <nikep.godbard@gmail.com>
2021-10-23 20:02:21 +0900

    配置変換

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // 球体アニメーションJS
-import { makeSquare } from './square.js'
-import { makeScene,addCube  } from './scene.js'
-import { makeGeometry } from './geometry.js'
-import { makeCustomGeometry } from './custom.js'
-import { makePropMesh } from './meshProp.js'
-import { makeMultiCamera } from './multiCamera.js'
-import { makeCameraLookAt } from './cameraLookAt.js';
+import { makeSquare } from './chap2JS/square.js'
+import { makeScene,addCube  } from './chap2JS/scene.js'
+import { makeGeometry } from './chap2JS/geometry.js'
+import { makeCustomGeometry } from './chap2JS/custom.js'
+import { makePropMesh } from './chap2JS/meshProp.js'
+import { makeMultiCamera } from './chap2JS/multiCamera.js'
+import { makeCameraLookAt } from './chap2JS/cameraLookAt.js';
 
 // ------------------------------------------------
 // グローバル変数
