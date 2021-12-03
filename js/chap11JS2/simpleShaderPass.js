311c08f63cd4fb376e5f43777bd0bec36bb0cc7c
hiro881213 <nikep.godbard@gmail.com>
2021-12-03 20:44:26 +0900

    SimpleShaderPassアニメーション

--- js/chap11JS2/simpleShaderPass.js
+++ js/chap11JS2/simpleShaderPass.js
@@ -87,7 +87,7 @@ export const makeSimpleShaderPass = () => {
     // 立方体メッシュを生成する
     let cube = new THREE.Mesh(
         plane,
-        new MeshPhongMaterial({
+        new THREE.MeshPhongMaterial({
             color: 0xffffff,
             map: textureLoader.load("../../assets/textures/general/plaster-diffuse.jpg"),
             normalMap: textureLoader.load("../../assets/textures/general/plaster-normal.jpg"),
@@ -108,7 +108,7 @@ export const makeSimpleShaderPass = () => {
     cube.position.x = -150;
 
     // シーンに立方体メッシュを追加する
-    cube.add(cube);
+    scene.add(cube);
 
     // -----------------------------------------
     // 立方体1メッシュ生成処理
@@ -175,7 +175,7 @@ export const makeSimpleShaderPass = () => {
     document.getElementById("simpleShadePass-output").appendChild(webGLRenderer.domElement);
 
     // ローダーを生成する
-    let mtlLoader = new THREE.MTLoader();
+    let mtlLoader = new THREE.MTLLoader();
 
     // パスを生成する
     mtlLoader.setPath("../../assets/models/sol/");
@@ -281,7 +281,7 @@ export const makeSimpleShaderPass = () => {
     // -----------------------------------------
 
     // colorifyを生成する
-    let colorify = new THREE.ShderPass(THREE.ColorifyShader);
+    let colorify = new THREE.ShaderPass(THREE.ColorifyShader);
 
     // 色を設定する
     colorify.uniforms.color.value = new THREE.Color(0xffffff);
@@ -384,32 +384,242 @@ export const makeSimpleShaderPass = () => {
         this.kalSides = 6;
         this.rotate = false;
 
+        // シェーダ変更処理
         this.switchShader = () => {
 
             switch (controls.select) {
+                
                 case 'none': {
                     enableShader();
                     break;
                 }
+                
                 case 'colorify': {
                     enableShader(colorify);
                     break;
                 }
+                
                 case 'brightness': {
                     enableShader(brightness);
                     break;
                 }
 
+                case 'sepia': {
+                    enableShader(sepia);
+                    break;
+                }
+
+                case 'colorCorrection': {
+                    enableShader(colorCorrection);
+                    break;
+                }
+
+                case 'rgbShift': {
+                    enableShader(rgbShift);
+                    break;
+                }
+
+                case 'mirror': {
+                    enableShader(mirror);
+                    break;
+                }
+                
+                case 'vignette': {
+                    enableShader(vignette);
+                    break;
+                }
+
+                case 'hueAndSaturation': {
+                    enableShader(hue);
+                    break;
+                }
+
+                case 'kaleidoscope': {
+                    enableShader(kal);
+                    break;
+                }
+
+                case 'luminosity': {
+                    enableShader(lum);
+                    break;
+                }
+
+                case 'technicolor': {
+                    enableShader(techni);
+                    break;
+                }
+
+                case 'unpackDepth': {
+                    enableShader(unpack);
+                    break;
+                }
 
             }
 
+        };
+
+        // 輝度変更処理
+        this.changeBrightness = () => {
+
+            brightness.uniforms.brightness.value = controls.brightness;
+            brightness.uniforms.contrast.value = controls.contrast;
+
+        };
+
+        // 色変更処理
+        this.changeColor = () => {
+
+            colorify.uniforms.color.value = new THREE.Color(controls.color);
+
+        };
+
+        // セピア変更処理
+        this.changeSepia = () => {
+
+            sepia.uniforms.amount.value = controls.amount;
+
+        };
+
+        // コレクション変更処理
+        this.changeCorrection = () => {
+
+            colorCorrection.uniforms.mulRGB.value = new THREE.Vendor3(controls.mulRGB_R, controls.mulRGB_G, controls.mulRGB_B);
+            colorCorrection.uniforms.powRGB.value = new THREE.Vendor3(controls.powRGB_R, controls.powRGB_G, controls.powRGB_B);
+
+        };
+
+        // RGBShifter変更処理
+        this.changeRGBShifter = () => {
+
+            rgbShift.uniforms.amount.value = controls.rgbAmount;
+            rgbShift.uniforms.angle.value = controls.angle;
+
+        };
+
+        // ミラー変更処理
+        this.changeMirror = () => {
+
+            mirror.uniforms.side.value = controls.side;
+
+        };
+
+        // Vignette変更処理
+        this.changeVignette = () => {
+
+            vignette.uniforms.darkness.value = controls.darkness;
+            vignette.uniforms.offset.value = controls.offset;
+
+        };
+
+        // hue変更処理
+        this.changeHue = () => {
+
+            hue.uniforms.hue.value = controls.hue;
+            hue.uniforms.saturation.value = controls.saturation;
+
+        };
+
+        // kal変更処理
+        this.changeKal = () => {
+
+            kal.uniforms.sides.value = controls.kalSides;
+            kal.uniforms.angle.value = controls.kalAngle;
+
+        };
+
+        function enableShader(shader) {
+
+            for (let i = 1; i < composer.passes.length - 1; i++) {
+
+                if (composer.passes[i] == shader) {
+
+                    composer.passes[i].enabled = true;
+
+                } else {
+
+                    composer.passes[i].enabled = false;
+
+                }
+
+            }
 
         }
 
+    };
+
+    // -----------------------------------------
+    // GUI生成処理
+    // -----------------------------------------
+
+    // GUIを生成する
+    let gui = new dat.GUI();
+
+    gui.add(controls, "select", 
+        ["none", "colorify", "brightness", "sepia", "colorCorrection", "rgbShift", "mirror", "vignette", "hueAndSaturation", "kaleidoscope", "luminosity", "technicolor"]).onChange(controls.switchShader);
+
+    gui.add(controls, "rotate");
+
+    const bnFolder = gui.addFolder("Brightness");
+    bnFolder.add(controls, "brightness", -1, 1).onChange(controls.changeBrightness);
+    bnFolder.add(controls, "contrast", -1, 1).onChange(controls.changeBrightness);
+
+    const clFolder = gui.addFolder("Colorify");
+    clFolder.add(controls, "color").onChange(controls.changeColor);
+
+    const colFolder = gui.addFolder("Color Correction");
+    colFolder.add(controls, "powRGB_R", 0, 5).onChange(controls.changeCorrection);
+    colFolder.add(controls, "powRGB_G", 0, 5).onChange(controls.changeCorrection);
+    colFolder.add(controls, "powRGB_B", 0, 5).onChange(controls.changeCorrection);
+    colFolder.add(controls, "mulRGB_R", 0, 5).onChange(controls.changeCorrection);
+    colFolder.add(controls, "mulRGB_G", 0, 5).onChange(controls.changeCorrection);
+    colFolder.add(controls, "mulRGB_B", 0, 5).onChange(controls.changeCorrection);
+
+    const sepiaFolder = gui.addFolder("Sepia");
+    sepiaFolder.add(controls, "amount", 0, 2).step(0.1).onChange(controls.changeSepia);
+
+    const shiftFolder = gui.addFolder("RGB Shift");
+    shiftFolder.add(controls, "rgbAmount", 0, 0.1).step(0.001).onChange(controls.changeRGBShifter);
+    shiftFolder.add(controls, "angle", 0, 3.14).step(0.001).onChange(controls.changeRGBShifter);
+
+    const mirrorFolder = gui.addFolder("mirror");
+    mirrorFolder.add(controls, "side", 0, 3).step(1).onChange(controls.changeMirror);
+
+    const vignetteFolder = gui.addFolder("vignette");
+    vignetteFolder.add(controls, "darkness", 0, 2).onChange(controls.changeVignette);
+    vignetteFolder.add(controls, "offset", 0, 2).onChange(controls.changeVignette);
+
+    const hueAndSat = gui.addFolder("hue and saturation");
+    hueAndSat.add(controls, "hue", -1, 1).step(0.01).onChange(controls.changeHue);
+    hueAndSat.add(controls, "saturation", -1, 1).step(0.01).onChange(controls.changeHue);
+
+    const kalMenu = gui.addFolder("Kaleidoscope");
+    kalMenu.add(controls, "kalAngle", -2*Math.PI, 2*Math.PI).onChange(controls.changeKal);
+    kalMenu.add(controls, "kalSides", 2, 20).onChange(controls.changeKal);
+
+    // レンダリング処理を実行する
+    render();
+
+    // -----------------------------------------
+    // レンダリング関数
+    // -----------------------------------------
+
+    function render() {
+
+        if (controls.rotate) {
+
+            if (mesh) mesh.rotation.y += 0.01;
+
+            // 立方体メッシュを回転させる
+            cube1.rotation.y += 0.01;
+            cube2.rotation.y += 0.01;
+            cube3.rotation.y += 0.01;
+
+        }
+
+        // アニメーションを生成する
+        requestAnimationFrame(render);
+        composer.render();
 
     }
-    
-
-
 
 }
